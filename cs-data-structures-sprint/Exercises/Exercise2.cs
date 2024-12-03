387f8de788dbae898d26ad9c5e9c15482d18b928
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-12-03 11:36:03 +0000

    Add LinkedList with PrintFirstAndLast

--- cs-data-structures-sprint/Exercises/Exercise2.cs
+++ cs-data-structures-sprint/Exercises/Exercise2.cs
@@ -8,16 +8,21 @@ namespace cs_data_structures_sprint.Exercises
 {
     internal class Exercise2
     {
-        public static void PrintFirstAndLast(LinkedList<string> list)
-        {
-            // write your code here
-        }
+        //public static void PrintFirstAndLast(LinkedList<string> list)
+        //{
+        //    // write your code here
+        //    Console.WriteLine("First Element : " + list.First.Value + " Last Element : " + list.Last.Value);
 
-        /*
+                
+        //}
+
+        
         public static void PrintFirstAndLast<T>(LinkedList<T> list)
         {
-            // challenge..?
+            //T firstElement =
+            Console.WriteLine("First Element : " + list.First.Value + " Last Element : " + list.Last.Value);
+
         }
-        */
+        
     }
 }
--- cs-data-structures-sprint/Program.cs
+++ cs-data-structures-sprint/Program.cs
@@ -1,3 +1,30 @@
 ﻿//Console.WriteLine("Your code can go here, if you like");
 using cs_data_structures_sprint.Exercises;
-Exercise1.PrintExesAndOhs();
+//Exercise1.PrintExesAndOhs();
+LinkedList<string> cars = new LinkedList<string>();
+cars.AddLast("Ford");
+cars.AddLast("Kia");
+cars.AddLast("BMW");
+cars.AddLast("Lexus");
+cars.AddLast("Skoda");
+cars.AddLast("Mercedes");
+cars.AddLast("Nissan");
+
+Exercise2.PrintFirstAndLast<string>(cars);
+
+//Exercise2.PrintFirstAndLast(cars);
+
+int[] intValues = { 1, 4, 6, 7, 8 };
+LinkedList<int> values = new LinkedList<int>(intValues);
+values.AddLast(10);
+values.AddBefore(values.Last, 200);
+Exercise2.PrintFirstAndLast(values);
+
+LinkedList<Car> newCars = new LinkedList<Car>();
+newCars.AddLast(new Car("Ford", "Icon", "Petrol"));
+newCars.AddLast(new Car("Kia", "Model1", "Disel"));
+newCars.AddLast(new Car("Nissan", "ModelA", "Petrol"));
+newCars.AddLast(new Car("Skoda", "ModelX", "Disel"));
+
+Exercise2.PrintFirstAndLast(newCars);
+
