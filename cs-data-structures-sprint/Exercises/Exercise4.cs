4d0d8af6a1feb32361aa78b25a8a3233e277dd6a
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-12-03 14:59:19 +0000

    update Task4 part1

--- cs-data-structures-sprint/Exercises/Exercise4.cs
+++ cs-data-structures-sprint/Exercises/Exercise4.cs
@@ -8,10 +8,22 @@ namespace cs_data_structures_sprint.Exercises
 {
     internal class Exercise4
     {
+        public static Dictionary<Student, int> StudentGrades = new();
+     
         
         public static void GradeStudent(Student student, int grade)
         {
-
+            StudentGrades.Add(student,grade);
+        }
+        public static int CheckGrade(Student student)
+        {
+            return StudentGrades[student];
+        }
+        public static void UpdateGrade(Student student, int grade)
+        {
+            StudentGrades[student] = grade;
+            
+            
         }
     }
 }
--- cs-data-structures-sprint/Program.cs
+++ cs-data-structures-sprint/Program.cs
@@ -1,26 +1,35 @@
 ﻿//Console.WriteLine("Your code can go here, if you like");
 using cs_data_structures_sprint.Exercises;
+var andrea = new Student("1", "Andrea");
+Exercise4.GradeStudent(andrea, 17);
+Console.WriteLine(Exercise4.CheckGrade(andrea));
+Exercise4.GradeStudent(andrea, 19);
+Console.WriteLine(Exercise4.CheckGrade(andrea));
+
+
+
+
 //Exercise1.PrintExesAndOhs();
-LinkedList<string> cars = new LinkedList<string>();
-cars.AddLast("Ford");
-cars.AddLast("Kia");
-cars.AddLast("BMW");
-cars.AddLast("Lexus");
-cars.AddLast("Skoda");
-cars.AddLast("Mercedes");
-cars.AddLast("Nissan");
+//LinkedList<string> cars = new LinkedList<string>();
+//cars.AddLast("Ford");
+//cars.AddLast("Kia");
+//cars.AddLast("BMW");
+//cars.AddLast("Lexus");
+//cars.AddLast("Skoda");
+//cars.AddLast("Mercedes");
+//cars.AddLast("Nissan");
 
-foreach (string car in cars)
-{
-    Console.WriteLine(car);
-}
+//foreach (string car in cars)
+//{
+//    Console.WriteLine(car);
+//}
 
-Console.WriteLine("\n\n\n");
-Exercise3.SwapElementPosition(cars, 1, 5);
-foreach (string car in cars)
-{
-    Console.WriteLine(car);
-}
+//Console.WriteLine("\n\n\n");
+//Exercise3.SwapElementPosition(cars, 1, 5);
+//foreach (string car in cars)
+//{
+//    Console.WriteLine(car);
+//}
 
 //Exercise2.PrintFirstAndLast<string>(cars);
 
