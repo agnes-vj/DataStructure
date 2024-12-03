fcdc5bdd9368ef2b93a6dc7f446e8f8e54b9618f
PomPomegranates <persephoneapplewhite@gmail.com>
2024-12-03 12:26:43 +0000

    Finally discovered ElementAt which is very helpful

--- cs-data-structures-sprint/Exercises/Exercise3.cs
+++ cs-data-structures-sprint/Exercises/Exercise3.cs
@@ -10,7 +10,67 @@ namespace cs_data_structures_sprint.Exercises
     {
         public static void SwapElementPosition(LinkedList<string> list, int index1, int index2)
         {
-            // Write your code here
+
+            int iterator = 0;
+
+            var current = list.First;
+
+            var index1String = list.ElementAt(index1);
+            var index2String = list.ElementAt(index2);
+
+            //LinkedListNode<string> node1Index;
+            //LinkedListNode<string> node2Index;
+
+            while (current.Next != null)
+            {
+                if (iterator == index1)
+                {
+                   current.Value = index2String;
+                   
+                }
+                if (iterator == index2)
+                {
+                    current.Value = index1String;
+                }
+                iterator++;
+                current = current.Next;
+            }
+            
+            
+
+
+
+
+
+
+
+
+
+            //int node = 0;
+            //foreach (string item in list)
+            //{
+            //    if (node == index1)
+            //    {
+
+            //    }
+            //    node++;
+
+            //}
+
+
+
+
+            // Find Index of 
+            // AddBefore()
+            // a, b , c, d, e, f, g, 
+            // 0, 1 , 2, 3, 4, 5, 6,
+            // swap c with f
+            // index 2 with index 5
+            // index 5 add before index 2 and remove from index index 6
+            // a, b, f, c, d, e,  g, 
+            // 0 , 1, 2, 3, 4, 5, 6,
+            // index 3 add before index 5 and remove index 3
+            //
         }
     }
 }
--- cs-data-structures-sprint/Program.cs
+++ cs-data-structures-sprint/Program.cs
@@ -10,21 +10,33 @@ cars.AddLast("Skoda");
 cars.AddLast("Mercedes");
 cars.AddLast("Nissan");
 
-Exercise2.PrintFirstAndLast<string>(cars);
+foreach (string car in cars)
+{
+    Console.WriteLine(car);
+}
 
-//Exercise2.PrintFirstAndLast(cars);
+Console.WriteLine("\n\n\n");
+Exercise3.SwapElementPosition(cars, 1, 5);
+foreach (string car in cars)
+{
+    Console.WriteLine(car);
+}
 
-int[] intValues = { 1, 4, 6, 7, 8 };
-LinkedList<int> values = new LinkedList<int>(intValues);
-values.AddLast(10);
-values.AddBefore(values.Last, 200);
-Exercise2.PrintFirstAndLast(values);
+//Exercise2.PrintFirstAndLast<string>(cars);
 
-LinkedList<Car> newCars = new LinkedList<Car>();
-newCars.AddLast(new Car("Ford", "Icon", "Petrol"));
-newCars.AddLast(new Car("Kia", "Model1", "Disel"));
-newCars.AddLast(new Car("Nissan", "ModelA", "Petrol"));
-newCars.AddLast(new Car("Skoda", "ModelX", "Disel"));
+////Exercise2.PrintFirstAndLast(cars);
 
-Exercise2.PrintFirstAndLast(newCars);
+//int[] intValues = { 1, 4, 6, 7, 8 };
+//LinkedList<int> values = new LinkedList<int>(intValues);
+//values.AddLast(10);
+//values.AddBefore(values.Last, 200);
+//Exercise2.PrintFirstAndLast(values);
+
+//LinkedList<Car> newCars = new LinkedList<Car>();
+//newCars.AddLast(new Car("Ford", "Icon", "Petrol"));
+//newCars.AddLast(new Car("Kia", "Model1", "Disel"));
+//newCars.AddLast(new Car("Nissan", "ModelA", "Petrol"));
+//newCars.AddLast(new Car("Skoda", "ModelX", "Disel"));
+
+//Exercise2.PrintFirstAndLast(newCars);
 
